385f767f3c98fc8a0ec76cfb8b88d596236be6e8
Eric Charles <eric@datalayer.io>
2023-10-20 17:51:54 +0200

    docs: readme

--- datalayer/__init__.py
+++ datalayer/__init__.py
@@ -2,12 +2,17 @@ from typing import Any, Dict, List
 
 from ._version import __version__
 from .serverapplication import DatalayerExtensionApp
+from .lab import DatalayerLabApp
 
 
 def _jupyter_server_extension_points() -> List[Dict[str, Any]]:
     return [{
         "module": "datalayer",
         "app": DatalayerExtensionApp,
+    },
+    {
+        "module": "datalayer",
+        "app": DatalayerLabApp,
     }]
 
 
