58262a311001b108663a172236f70c6509e857b9
Eric Charles <eric@datalayer.io>
2024-09-15 18:46:34 +0200

    chore: migrate to @datalayer/ui

--- src/jupyterlab/index.ts
+++ src/jupyterlab/index.ts
@@ -1,13 +1,13 @@
 import {
-    activatorPlugin,
-    datalayerCorePlugin,
-    jupyterIAMPlugin,
-    jupyterKernelsPlugin,
-} from '@datalayer/run';
+  activatorPlugin,
+  datalayerCorePlugin,
+  jupyterIAMPlugin,
+  jupyterKernelsPlugin,
+} from '@datalayer/ui';
 
 export default [
-    activatorPlugin,
-    datalayerCorePlugin,
-    jupyterIAMPlugin,
-    ...jupyterKernelsPlugin,
+  activatorPlugin,
+  datalayerCorePlugin,
+  jupyterIAMPlugin,
+  ...jupyterKernelsPlugin,
 ];
