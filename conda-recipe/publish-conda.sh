e9ae8154d03bde2e1a3db5f8427eb875e4c6f670
Eric Charles <eric@datalayer.io>
2024-12-04 05:11:51 +0100

    build: conda

---
+++ conda-recipe/publish-conda.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+export DATALAYER_CORE_VERSION=1.0.23
+
+CONDA_CHANNEL_NAME=datalayer
+ORGANIZATION=datalayer
+
+# Make sure the script stops on any error (errors are otherwise hard to spot)
+set -o errtrace -o nounset -o pipefail -o errexit
+
+rm -fr ./out
+
+PKG_PATH=$(conda-build --output-folder ./out . -c defaults)
+
+echo $PKG_PATH generated
+
+# Upload the package to a conda channel.
+# anaconda upload $PKG_PATH -c $CONDA_CHANNEL_NAME
+# Upload the package to a conda organization.
+anaconda upload --user $ORGANIZATION $PKG_PATH
