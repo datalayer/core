99119925b11ab30777115257e6a1881a3922ef41
Eric Charles <eric@datalayer.io>
2025-03-06 11:20:15 +0100

    rel: 1.0.27 (#42)
    * rel: 1.0.26
    
    * rel: 1.0.27
    
    * rel: 1.0.27

--- conda-recipe/publish-conda.sh
+++ conda-recipe/publish-conda.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2023-2024 Datalayer, Inc.
 # Distributed under the terms of the Modified BSD License.
 
-export DATALAYER_CORE_VERSION=1.0.24
+export DATALAYER_CORE_VERSION=1.0.27
 
 CONDA_CHANNEL_NAME=datalayer
 
@@ -14,12 +14,9 @@ set -o errtrace -o nounset -o pipefail -o errexit
 
 rm -fr ./out
 
-PKG_PATH=$(conda-build --output-folder ./out . -c defaults)
+PKG_PATH=$(conda-build --output-folder ./out . --user $ORGANIZATION -c defaults -c $CONDA_CHANNEL_NAME)
 
 echo $PKG_PATH generated
-
-# Upload the package to a conda channel.
-# anaconda upload $PKG_PATH -c $CONDA_CHANNEL_NAME
 
 # Upload the package to a conda organization.
 anaconda upload --user $ORGANIZATION $PKG_PATH
--- datalayer_core/__version__.py
+++ datalayer_core/__version__.py
@@ -1,4 +1,4 @@
 # Copyright (c) 2023-2024 Datalayer, Inc.
 # Distributed under the terms of the Modified BSD License.
 
-__version__ = '1.0.25'
+__version__ = '1.0.27'
