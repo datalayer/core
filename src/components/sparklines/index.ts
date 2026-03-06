8f4cdd8d15731d2a35d1770aaf1982cde0030cdb
Eric Charles <eric@datalayer.io>
2026-03-06 14:30:15 +0100

    sparklines

---
+++ src/components/sparklines/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2025-2026 Datalayer, Inc.
+ * Distributed under the terms of the Modified BSD License.
+ */
+
+export { Sparklines } from './Sparklines';
+export { SparklinesLine } from './SparklinesLine';
+export type { SparklinesProps, SparklinesLineProps, Point } from './types';
+export { dataToPoints, min, max } from './dataProcessing';
